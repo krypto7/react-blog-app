60809f872899c25f56412e6d58ead46da5b6a914
nishantkarekar <nishantkarekar98@gmail.com>
2025-03-12 03:21:09 +0530

    add env.example file

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_REACT_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+  appwriteBacketId: String(import.meta.env.VITE_BUCKET_ID),
+};
+export default conf;
